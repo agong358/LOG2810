22121c9e8150fdc33929944a5968a96515947af0
Alice Gong <ecila.358@gmail.com>
2019-10-24 10:39:13 -0400

    ajout de todo et des headers des fonctions

--- TP1/Implementation.java
+++ TP1/Implementation.java
@@ -1,2 +1,36 @@
-import java.util
-        
+import java.util.*;
+
+/** TODO
+ * Créer un graphe représentant les différentes section de l'entrepôt, ainsi
+ * que les chemins reliants ces sections entre elles à partir d'un fichier texte.
+ */
+private void creerGraphe(){}
+
+
+/** TODO
+ * Affiche la représentation du graphe sauvegardé en mémoire
+ */
+private void afficherGraphe(){}
+
+/** TODO
+ * Permet à l’utilisateur de rentrer un nombre d'objets de chaque
+ * type que le robot devra aller chercher.
+ */
+private void prendreCommande(){}
+
+/** TODO
+ * Permet de voir la commande en mémoire.
+ */
+private void afficherCommande(){}
+
+/** TODO
+ * Affiche le type de robot utilisé, la liste des noeuds traversés,
+ * spécifie lorsqu'un objet est pris à un noeud et le temps
+ * total que le robot a pris pour aller chercher la commande.
+ *
+ * Si le chemin est impossible, la fonction doit en informer l'utilisateur.
+ */
+private void plusCourtChemin(){}
+
+
+private void quitter(){}

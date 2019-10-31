01ed6f6b94e8c74e46a8a5e4bc1336ca0af4bb21
Alice Gong <ecila.358@gmail.com>
2019-10-31 11:39:59 -0400

    Début implémentation des méthodes de Commande.java

--- TP1/Commande.java
+++ TP1/Commande.java
@@ -1,11 +1,39 @@
 package LOG2810.TP1;
 
-public class Commande {
-    int nbObjets1, nbObjets2, nbObjets3;
+import java.util.Scanner;
 
-    void Commmande(){
-        nbObjets1 = 0;
-        nbObjets2 = 0;
-        nbObjets3 = 0;
+public class Commande {
+    int nbObjetsA_, nbObjetsB_, nbObjetsC_;
+
+    public void Commmande(){
+        nbObjetsA_ = 0;
+        nbObjetsB_ = 0;
+        nbObjetsC_ = 0;
     }
+
+    /** TODO
+     * Permet à l’utilisateur de rentrer un nombre d'objets de chaque
+     * type que le robot devra aller chercher.
+     */
+    public void prendreCommande(){
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 3; i ++) {
+            System.out.println("Entrez le nombre d'objets de type A: ");
+        }
+
+
+//        nbObjetsA_ = nbObjetsA;
+//        nbObjetsB_ = nbObjetsB;
+//        nbObjetsC_ = nbObjetsC;
+    }
+
+    /** TODO
+     * Permet de voir la commande en mémoire.
+     */
+    public void afficherCommande(){
+        System.out.println(nbObjetsA_ + nbObjetsB_+ nbObjetsC_);
+    }
+
+
+
 }

d23fd91276c4883e6b7337f04b1ce9d2a22ddcb4
Alice Gong <ecila.358@gmail.com>
2019-10-31 09:59:18 -0400

    Création fichier Commnade

---
+++ TP1/Commande.java
@@ -0,0 +1,11 @@
+package LOG2810.TP1;
+
+public class Commande {
+    int nbObjets1, nbObjets2, nbObjets3;
+
+    void Commmande(){
+        nbObjets1 = 0;
+        nbObjets2 = 0;
+        nbObjets3 = 0;
+    }
+}

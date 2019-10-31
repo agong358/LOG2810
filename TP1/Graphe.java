d188303a4860b69707fb15df02f39a5b01daa1a6
Nhien Ton <nhienton07@gmail.com>
2019-10-31 09:57:20 -0400

    commence interface et classes dans graphes, debut scanner

--- TP1/Graphe.java
+++ TP1/Graphe.java
@@ -1,18 +1,40 @@
-package LOG2810.TP1;
+
 
 import java.util.*;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Graphe {
+    private Sommet[] liste;
 
     // constructeur par defaut
     public Graphe(){
-
+        liste = null;
     }
     /** TODO
      * Créer un graphe représentant les différentes section de l'entrepôt, ainsi
      * que les chemins reliants ces sections entre elles à partir d'un fichier texte.
      */
-    private void creerGraphe(String fichier){
+    public void creerGraphe(String fichier){
+//        File file = new File(fichier);
+//        try {
+//            Scanner scan = new Scanner(file);
+//        }
+//        catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+        Scanner scan = new Scanner("0,0,0,0 \n" +
+                "0,1,1,1");
+        scan.useDelimiter(",");
+        String allo = new String("0,0,0,0 \n 0,0" +
+                "0,1,1,1");
+
+        while(scan.hasNext()) {
+            System.out.println(scan.next());
+        }
+        //System.out.println(scan.next());
+        System.out.println(allo);
 
     }
 
@@ -43,4 +65,30 @@ public class Graphe {
 
 
     private void quitter(){}
+
+    private void afficherInterface() {
+        System.out.println("Veuillez sélectionner une option");
+        System.out.println("[] Créer le graphe");
+        System.out.println("[] Afficher le graphe");
+        System.out.println("[] Prendre une commande");
+        System.out.println("[] Afficher la commande");
+        System.out.println("[] Trouver le plus court chemin");
+        System.out.println("[] Quitter");
+    }
+
+    static class Voisin {
+        int noeudVoisin;
+        int distance;
+    }
+
+    static class Sommet {
+        int noeud;
+        int nbObjetsA = 0;
+        int nbObjetsB = 0;
+        int nbObjetsC = 0;
+        Voisin[] voisins = null;
+    }
+
+
+
 }
--- TP1/Main.java
+++ TP1/Main.java
@@ -1,3 +1,14 @@
 public class Main {
-
+    public static void main(String[] args) {
+        System.out.println("Bienvenue au troisieme labo de INF2010!");
+        Graphe graphe = new Graphe();
+        System.out.println("Veuillez sélectionner une option");
+        System.out.println("[] Créer le graphe");
+        System.out.println("[] Afficher le graphe");
+        System.out.println("[] Prendre une commande");
+        System.out.println("[] Afficher la commande");
+        System.out.println("[] Trouver le plus court chemin");
+        System.out.println("[] Quitter");
+        graphe.creerGraphe("fichier");
+    }
 }

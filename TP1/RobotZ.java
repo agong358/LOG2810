aabbd603d0ef98de7000036ed1609a1aae4ffd39
Kai Sen Trieu <kai.sen.trieu@gmail.com>
2019-11-05 23:11:42 -0500

    commentaire robotXYZ

--- TP1/RobotZ.java
+++ TP1/RobotZ.java
@@ -1,13 +1,29 @@
+/**
+ * Classe pour contenant des méthodes pour le robot Z
+ *
+ * @auteure: Alice Gong
+ * @auteure: Nu Chan Nhien Ton
+ * @auteure: Kai Sen Trieu
+ */
 public class RobotZ extends Robots{
     private int chargeMax = 25;
 
-    //Calculer la constante pour robot z
+    /**
+     * Calcule la constante pour le robot Z
+     *
+     * @param masse masse des objets portés par le robot
+     * @return cstK
+     */
     public double calculerCstK(double masse) {
         cstK = 2.5 + 0.2 * masse;
         return cstK;
     }
 
-    //Constructeur par paramètres
+    /**
+     * Contructeur par paramètres
+     *
+     * @param commande données entrées par l'utilisateur
+     */
     public RobotZ(Commande commande) {
         setCharge(chargeMax);
         this.commande = commande;

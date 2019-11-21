da49b4aa0558df30660a04aadebbbac2e60636a2
Nhien Ton <nhienton07@gmail.com>
2019-11-21 10:40:41 -0500

    kjdskjg

--- TP2/Main.java
+++ TP2/Main.java
@@ -1,11 +1,7 @@
 public class Main {
-<<<<<<< HEAD
     public static void main(String[] args){
         InterfaceUsager interfaceUsager = new InterfaceUsager();
         interfaceUsager.interfaceUsager();
-=======
-    public static void main(String[] args) {
->>>>>>> 700607528e215d0ccdf7df993fbed82e2adbe533
 
     }
 }
